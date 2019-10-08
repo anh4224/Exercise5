f1b617f008f8ef702adaee70e82565162c1fdffe
quanganh <bantia447@gmail.com>
2019-10-08 17:25:21 +0700

    Write

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print_a_2_z_v1();
+            Print_a_2_z_v4();
+        }
+
+
+        public static void Print_a_2_z_v1()
+        {
+            char c = ' ';
+            for (int i = 97; i <= 122; i++)
+            {
+                c = (char)i;
+                Console.Write(c);
+            }
+
+        }
+        public static void Print_a_2_z_v4()
+        {
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                Console.Write(c);
+            }
+
+        }
+
+    }
+}
